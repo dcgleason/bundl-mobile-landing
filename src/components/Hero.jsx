49089989b0d6bbf63c9792de1fd1d10904711fb0
dcgleason <danny.c.gleason@gmail.com>
2023-08-19 16:36:10 -0400

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -105,7 +105,7 @@ export function Hero() {
         <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20">
           <div className="relative z-10 mx-auto max-w-2xl lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6">
             <h1 className="text-4xl font-medium tracking-tight text-gray-900">
-              Include family and friends in your proposal. 
+              Include your partner&apos;s family and friends in your proposal, remotely. 
             </h1>
             <p className="mt-6 text-lg text-gray-600">
              Bundl Book: A surprise gift book filled with loving notes and pictures from your partner&apos;s family and friends. Let them join your big moment.
