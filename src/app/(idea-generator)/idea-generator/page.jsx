4a04d3dbf4dda63f1878b0a147bf7bc000e79dd2
dcgleason <danny.c.gleason@gmail.com>
2023-08-21 09:29:03 -0400

    update grid system

--- src/app/(idea-generator)/idea-generator/page.jsx
+++ src/app/(idea-generator)/idea-generator/page.jsx
@@ -168,7 +168,7 @@ async function handleSubmit(e) {
         </p>
       </div>
       <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
-        <div className="grid grid-cols-1 gap-y-6 gap-x-8 sm:grid-cols-2">
+        <div className="grid grid-cols-1 gap-y-6 gap-x-8">
           <div>
             <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
             How did you and your partner meet?
