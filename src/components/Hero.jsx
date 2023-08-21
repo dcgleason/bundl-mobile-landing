1548737c6deb089f2bc85b69bcb54d9d3bbd46a1
dcgleason <danny.c.gleason@gmail.com>
2023-08-21 16:10:34 -0400

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -140,7 +140,7 @@ export function Hero() {
         <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20">
           <div className="relative z-10 mx-auto max-w-2xl lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6">
             <h1 className="text-4xl font-medium tracking-tight text-gray-900">
-            Tears of joy when you pop the question, or your money back.
+            Tears of joy from your partner when you pop the question, or your money back.
             </h1>
             <p className="mt-6 text-lg text-gray-600">
              Bundl Book: A surprise proposal gift book packed with loving notes and pictures from your partner&apos;s family and friends.
