52b02f802ce7236551d5f70a814b06a83115cee7
dcgleason <danny.c.gleason@gmail.com>
2023-08-21 12:53:01 -0400

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -152,7 +152,7 @@ export function Hero() {
                 variant="outline"
               >
                 <PlayIcon className="h-6 w-6 flex-none" />
-                <span className="ml-2.5">Video to inspire confidence</span>
+                <span className="ml-2.5">Loosen up, you got this!</span>
               </Button>
             </div>
           </div>
