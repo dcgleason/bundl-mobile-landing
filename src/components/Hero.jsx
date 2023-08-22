f9ab8d0890edad891e3d9e5cdf7fd0e260dc611b
dcgleason <danny.c.gleason@gmail.com>
2023-08-22 08:41:51 -0400

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -140,8 +140,8 @@ export function Hero() {
         <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20">
           <div className="relative z-10 mx-auto max-w-2xl lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6">
             <h1 className="text-4xl font-medium tracking-tight text-gray-900">
-            Elicit tears of joy when you propose, or your money back.           
-            
+            Make them cry happy tears when you ask them to marry you, or you get your money back.
+                        
            </h1>
             <p className="mt-6 text-lg text-gray-600">
              Bundl Book: A surprise proposal gift book to your partner packed with loving notes and pictures from your partner&apos;s family and friends.
