25eb81b22c7ff75c2ab24ece233a485bdaeb68bd
dcgleason <danny.c.gleason@gmail.com>
2023-08-28 08:38:24 -0400

    updated added sitemap

---
+++ src/app/(sitemap)/sitemap.xml/page.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Sitemap = () => {
+  return null;
+};
+
+export const getServerSideProps = async ({ res }) => {
+  const BASE_URL = 'https://www.givebundl.com';
+
+  const sitemap = `
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+        <loc>${BASE_URL}/</loc>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+      </url>
+      <url>
+        <loc>${BASE_URL}/idea-generation</loc>
+        <changefreq>daily</changefreq>
+        <priority>0.8</priority>
+      </url>
+    </urlset>
+  `;
+
+  res.setHeader('Content-Type', 'text/xml');
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+};
+
+export default Sitemap;
