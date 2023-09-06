3045e34ba5a583fe4277c9849d8c4b10331564b7
dcgleason <danny.c.gleason@gmail.com>
2023-09-06 11:13:51 -0400

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -140,11 +140,11 @@ export function Hero() {
         <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20">
           <div className="relative z-10 mx-auto max-w-2xl lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6">
             <h1 className="text-4xl font-medium tracking-tight text-gray-900">
-            Your best gift...ever. 
+            Your most thoughtful gift...ever. 
 
            </h1>
             <p className="mt-6 text-lg text-gray-600">
-             Bundl Book: A surprise proposal book filled with loving notes and pictures from the recipient&apos;s family and friends. Great for birthdays, babyshowers, retirement parties, etc.
+             Bundl Book: A surprise gift book filled with loving notes and pictures from the recipient&apos;s family and friends. Great for birthdays, babyshowers, retirement parties, etc.
             </p>
             <div className="mt-8 flex flex-wrap gap-x-6 gap-y-4">
               <AppStoreLink />
