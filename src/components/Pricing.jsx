3eab2242c809e95c2083abc89d2b9d408f2b9bee
dcgleason <danny.c.gleason@gmail.com>
2023-09-08 13:04:41 -0400

    update

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -187,7 +187,7 @@ export function Pricing() {
   <div class="mx-auto max-w-7xl px-6 lg:px-8">
     <div class="mx-auto max-w-2xl sm:text-center">
       <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Get your gift book in-app.</h2>
-      <p class="mt-6 text-lg leading-8 text-gray-600">Your gift recipient cries tears of joy or your money back... (we&apos;re serious).</p>
+      <p class="mt-6 text-lg leading-8 text-gray-600">Your gift recipient cries tears of joy or your money back... (we&apos;re serious about this).</p>
     </div>
     <div class="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
       <div class="p-8 sm:p-10 lg:flex-auto">
