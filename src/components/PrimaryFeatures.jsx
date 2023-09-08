e98f2f7fe042fdd141ee1c2c30d94a52d7958a5a
dcgleason <danny.c.gleason@gmail.com>
2023-09-08 15:09:03 -0400

    update

--- src/components/PrimaryFeatures.jsx
+++ src/components/PrimaryFeatures.jsx
@@ -483,7 +483,7 @@ export function PrimaryFeatures() {
          Create your Bundl book in 5 minutes with our app.
           </h2>
           <p className="mt-2 text-lg text-gray-400">
-          Select from your contacts and our mobile app gathers notes and pictures from your recipient&apos;s loved ones during the first week. During week two, your book is printed and shipped directly to you.
+          Select from your contacts and our mobile app automatically gathers notes and pictures from your recipient&apos;s loved ones during the first week. During week two, your book is printed and shipped directly to you.
           </p>
         </div>
       </Container>
