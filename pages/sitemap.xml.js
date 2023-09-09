a6f505047e3f94f6a0f5263c345e24d929b68038
dcgleason <danny.c.gleason@gmail.com>
2023-09-09 13:10:12 -0400

    update to sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -14,11 +14,6 @@ export const getServerSideProps = async ({ res }) => {
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
       </url>
-      <url>
-        <loc>${BASE_URL}/idea-generator</loc>
-        <changefreq>daily</changefreq>
-        <priority>0.8</priority>
-      </url>
     </urlset>
   `;
 
