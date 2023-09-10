14b93ec31b7ec802306ecf98eb7e03a991b0ab2b
dcgleason <danny.c.gleason@gmail.com>
2023-09-10 18:45:04 -0400

    updaet

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -140,7 +140,7 @@ export function Hero() {
         <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20">
           <div className="relative z-10 mx-auto max-w-2xl lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6">
             <h1 className="text-4xl font-medium tracking-tight text-gray-900">
-            Happy tears or your money back.
+            Make them cry happy tears or your money back.
            </h1>
             <p className="mt-6 text-lg text-gray-600">
              Give a Bundl book, a.k.a. a surprise, physical gift book for a friend or family member packed with celebratory notes, audio, memorable pictures from <em>their</em> family and friends. Great for birthdays, babyshowers, retirements, etc.
