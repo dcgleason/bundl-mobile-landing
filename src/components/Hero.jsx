c7fd3134a0b304968b54732ad382039cf34c4756
dcgleason <danny.c.gleason@gmail.com>
2023-09-09 20:29:09 -0400

    uodate

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -140,7 +140,7 @@ export function Hero() {
         <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20">
           <div className="relative z-10 mx-auto max-w-2xl lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6">
             <h1 className="text-4xl font-medium tracking-tight text-gray-900">
-            Stop being so selfish! Give a Bundl book to show you <em>actually</em> give a sh*t.
+           Give a Bundl book to show you <em>actually</em> give a sh*t.
            </h1>
             <p className="mt-6 text-lg text-gray-600">
              Bundl Book: A way to elicit happy tears with 5 minutes of work .... a.k.a. a surprise, physical gift book to your loved one packed with celebratory notes + audio + memorable pictures from <em>their</em> family and friends. Great for birthdays, babyshowers, retirements, etc.
