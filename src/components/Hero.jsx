2bb82e63eb8c04b39e9b777b8af73bbc042c98ef
dcgleason <danny.c.gleason@gmail.com>
2023-09-11 11:58:27 -0400

    update tranparent

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -158,7 +158,7 @@ export function Hero() {
           </div>
           <div className="relative mt-10 sm:mt-20 lg:col-span-5 lg:row-span-2 lg:mt-0 xl:col-span-6">
             <BackgroundIllustration className="absolute left-1/2 top-4 h-[1026px] w-[1026px] -translate-x-1/3 stroke-gray-300/70 [mask-image:linear-gradient(to_bottom,white_20%,transparent_75%)] sm:top-16 sm:-translate-x-1/2 lg:-top-16 lg:ml-12 xl:-top-14 xl:ml-0" />
-            <div className="-mx-4 px-9 [mask-image:linear-gradient(to_bottom,white_60%,transparent)] sm:mx-0 lg:absolute lg:-inset-x-10 lg:bottom-auto lg:-top-30 lg:h-auto lg:px-0 lg:pt-10 xl:bottom-0">
+            <div className="-mx-4 px-9 [mask-image:linear-gradient(to_bottom,white_10%,transparent)] sm:mx-0 lg:absolute lg:-inset-x-10 lg:bottom-auto lg:-top-30 lg:h-auto lg:px-0 lg:pt-10 xl:bottom-0">
               <Image 
                 src="https://upload.wikimedia.org/wikipedia/commons/7/70/Elizaemailtwo.jpg" 
                 alt="Your image description" 
