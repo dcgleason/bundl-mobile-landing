a5a2e172bbb9b5b522bb5b3c253fc63c8b0c8f42
dcgleason <danny.c.gleason@gmail.com>
2023-09-11 10:07:35 -0400

    update

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -191,8 +191,8 @@ export function Pricing() {
     </div>
     <div class="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
       <div class="p-8 sm:p-10 lg:flex-auto">
-        <h3 class="text-2xl font-bold tracking-tight text-gray-900">Bundl Book</h3>
-        <p class="mt-6 text-base leading-7 text-gray-600">Linen hardback with unlimited pages. Ability for each contributor to record an audio clip, which appears as a QR code next to his/her note, included.</p>
+        <h3 class="text-2xl font-bold tracking-tight text-gray-900">Free Email Series Bundl</h3>
+        <p class="mt-6 text-base leading-7 text-gray-600">Automated, stylized, and spaced out for maximum impact.</p>
         <div class="mt-10 flex items-center gap-x-4">
           <h4 class="flex-none text-sm font-semibold leading-6 text-black">What&apos;s included</h4>
           <div class="h-px flex-auto bg-gray-100"></div>
@@ -202,41 +202,40 @@ export function Pricing() {
             <svg class="h-6 w-5 flex-none text-black" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
             </svg>
-            Linen hardback quality book
+            Automated submission collection
           </li>
           <li class="flex gap-x-3">
             <svg class="h-6 w-5 flex-none text-black" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
             </svg>
-            Unlimited number of pages
+            Stylized emails
           </li>
           <li class="flex gap-x-3">
             <svg class="h-6 w-5 flex-none text-black" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
             </svg>
-            Two-week delivery (for collection, printing, and shipping)
+            Spaced out delivery (daily)
             </li>
           <li class="flex gap-x-3">
             <svg class="h-6 w-5 flex-none text-black" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
             </svg>
-           Option for audio clips (QR codes) included
+            Personalized messaging from you
           </li>
         </ul>
       </div>
       <div class="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
-        <div class="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
-          <div class="mx-auto max-w-xs px-8">
-            {/* <p class="text-base font-semibold text-gray-600">Pay once, own it forever</p> */}
-            <p class="mt-6 flex items-baseline justify-center gap-x-2">
-              <span class="text-5xl font-bold tracking-tight text-gray-900">$179</span>
-              <span class="text-sm font-semibold leading-6 tracking-wide text-gray-600">USD</span>
-            </p>
-            <a href="#" class="mt-10 block w-full rounded-md bg-[#DF9496] px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-600">Join waitlist</a>
-            <p class="mt-6 text-xs leading-5 text-gray-600"> Your most meaningful gift ever... now free.</p>
+          <div class="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
+            <div class="mx-auto max-w-xs px-8">
+              <p class="mt-6 flex items-baseline justify-center gap-x-2">
+                <span class="text-5xl font-bold tracking-tight text-gray-900 line-through">$25</span>
+                <span class="text-2xl font-bold text-gray-900">FREE</span>
+              </p>
+              <a href="#" class="mt-10 block w-full rounded-md bg-[#DF9496] px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-600">Join waitlist</a>
+              <p class="mt-6 text-xs leading-5 text-gray-600">Your most meaningful gift ever... now free.</p>
+            </div>
           </div>
         </div>
-      </div>
     </div>
   </div>
 </div>
