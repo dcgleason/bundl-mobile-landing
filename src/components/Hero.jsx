b7fc01407502f19dfce1693e41f64651287fa977
dcgleason <danny.c.gleason@gmail.com>
2023-09-13 16:28:05 -0400

    update added letter

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -161,7 +161,7 @@ export function Hero() {
             <BackgroundIllustration className="absolute left-1/2 top-4 h-[1026px] w-[1026px] -translate-x-1/3 stroke-gray-300/70 [mask-image:linear-gradient(to_bottom,white_20%,transparent_75%)] sm:top-16 sm:-translate-x-1/2 lg:-top-16 lg:ml-12 xl:-top-14 xl:ml-0" />
             <div className="mx-4 px-9 [mask-image:linear-gradient(to_bottom,white_95%,transparent)] sm:mx-0 lg:absolute lg:-inset-x-10 lg:bottom-auto lg:-top-20 lg:h-auto lg:px-0 xl:bottom-0">
               <Image 
-                src="https://upload.wikimedia.org/wikipedia/commons/7/70/Elizaemailtwo.jpg" 
+                src="https://upload.wikimedia.org/wikipedia/commons/f/f6/Letterbundl.jpg" 
                 alt="Your image description" 
                 layout="responsive" 
                 height={341} 
