84701ca486ee3678de39c3077200a04d9edbf3c9
dcgleason <danny.c.gleason@gmail.com>
2023-09-13 20:23:04 -0400

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -140,7 +140,7 @@ export function Hero() {
         <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20">
           <div className="relative z-10 mx-auto max-w-2xl lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6">
             <h1 className="text-4xl font-medium tracking-tight text-gray-900">
-            Gauranteed to tug at their heartstrings...in only 5 minutes!
+            Gauranteed to tug at their heartstrings.
            </h1>
             <p className="mt-6 text-lg text-gray-600">
             Give your friend or family member a FREE <em>&apos;Bundl&apos;</em> (a.k.a send them a daily email, each filled with a special, prompt-driven written note, audio-clip, and/or memorable picture each collected from friends and family using our mobile app)! Great for leading up to birthdays, babyshowers, retirements, etc.
