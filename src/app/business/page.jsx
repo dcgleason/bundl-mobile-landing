7f15b5b8fc4223b0ab1451b556e21eb256257e21
dcgleason <danny.c.gleason@gmail.com>
2023-09-21 11:34:56 -0400

    added pdf source

--- src/app/business/page.jsx
+++ src/app/business/page.jsx
@@ -144,18 +144,19 @@ function Services() {
             </FadeIn>
           </div>
           <List className="mt-16 lg:mt-0 lg:w-1/2 lg:min-w-[33rem] lg:pl-4">
-            <ListItem title="Skyrocket Productivity">
-              Boost productivity by 17% as per a Gallup study. More productivity means more growth. <a href="https://news.gallup.com/businessjournal/200108/damage-inflicted-poor-managers.aspx#:~:text=17%25%20higher%20productivity" target="_blank" rel="noopener noreferrer">[Source]</a>
+            <ListItem title="Boost Productivity by 17%">
+              A Gallup study shows a 17% increase in productivity with effective recognition. <a href="https://news.gallup.com/businessjournal/200108/damage-inflicted-poor-managers.aspx#:~:text=17%25%20higher%20productivity" target="_blank" rel="noopener noreferrer">[Source]</a>
             </ListItem>
-            <ListItem title="Cultivate Culture">
-             Employees are 5x as likely to feel connected to their work culture with recognition programs, says Workhuman. Culture is your competitive edge.  <a href="https://www.workhuman.com/resources/reports-guides/unleashing-the-human-element-at-work-transforming-workplaces-through-recognition/" target="_blank" rel="noopener noreferrer">[Source]</a>
+            <ListItem title="Enhance Work Culture 5x">
+              Workhuman states that recognition programs make employees 5x more likely to feel connected to their work culture. <a href="https://www.workhuman.com/resources/reports-guides/unleashing-the-human-element-at-work-transforming-workplaces-through-recognition/" target="_blank" rel="noopener noreferrer">[Source]</a>
             </ListItem>
-            <ListItem title="Slash Costs">
-            High-recognition companies cut voluntary turnover rates by 31%, and save on the cost to replace talent, according to a recent Deliote study. <a href="https://www2.deloitte.com/us/en/insights/deloitte-review/issue-16/employee-engagement-strategies.html" target="_blank" rel="noopener noreferrer">[Source]</a>
+            <ListItem title="Reduce Turnover by 31%">
+              According to Deloitte, high-recognition companies cut voluntary turnover rates by 31%. <a href="https://www2.deloitte.com/us/en/insights/deloitte-review/issue-16/employee-engagement-strategies.html" target="_blank" rel="noopener noreferrer">[Source]</a>
             </ListItem>
-            <ListItem title="ROI You Can See">
-            Employees who don&apos;t receive fulfilling recognition are 4x more likely to be actively looking for another job. But when recognition hits the mark, 56% of employees are less likely to be looking for job opportunities.<a href="https://www.workhuman.com/blog/effective-employee-retention-strategies/" target="_blank" rel="noopener noreferrer">[Source]</a>           
-             </ListItem>
+            <ListItem title="Boost Wellbeing with Authentic Recognition">
+            When employees strongly agree that the recognition they receive is authentic, they are 6-8X more likely to strongly agree that their organization cares about their wellbeing. <a href="/GallupRecognitionPaper.pdf" target="_blank" rel="noopener noreferrer">[Source]</a>
+          </ListItem>
+
           </List>
         </div>
       </Container>
