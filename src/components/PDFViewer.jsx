e9ab35f4bb3101da4144454ed303d780649b7d2d
dcgleason <danny.c.gleason@gmail.com>
2023-09-22 12:56:54 -0400

    update mobile pdf

--- src/components/PDFViewer.jsx
+++ src/components/PDFViewer.jsx
@@ -1,11 +1,11 @@
-import { Worker, Viewer } from '@react-pdf-viewer/core';
+import { Worker, Viewer, ScrollMode } from '@react-pdf-viewer/core';
 
 const PDFViewer = ({ fileUrl }) => {
-    return (
-      <Worker workerUrl={`https://unpkg.com/pdfjs-dist@2.16.105/build/pdf.worker.min.js`}>
-        <Viewer fileUrl={fileUrl} />
-      </Worker>
-    );
-  };
+  return (
+    <Worker workerUrl={`https://unpkg.com/pdfjs-dist@2.16.105/build/pdf.worker.min.js`}>
+      <Viewer fileUrl={fileUrl} defaultScale={1} scrollMode={ScrollMode.Vertical} />
+    </Worker>
+  );
+};
 
 export default PDFViewer;
