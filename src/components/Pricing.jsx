e73203b5aced271648ee5a0e284ba36821d4f5fa
dcgleason <danny.c.gleason@gmail.com>
2023-09-26 19:26:26 -0400

    update

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -191,8 +191,8 @@ export function Pricing() {
     </div>
     <div class="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
       <div class="p-8 sm:p-10 lg:flex-auto">
-        <h3 class="text-2xl font-bold tracking-tight text-gray-900">Email Series Bundl</h3>
-        <p class="mt-6 text-base leading-7 text-gray-600">Automated, stylized, and spaced out for maximum impact.</p>
+        <h3 class="text-2xl font-bold tracking-tight text-gray-900">Digital Bundl</h3>
+        <p class="mt-6 text-base leading-7 text-gray-600">Automated, stylized, and built out for emotional impact.</p>
         <div class="mt-10 flex items-center gap-x-4">
           <h4 class="flex-none text-sm font-semibold leading-6 text-black">What&apos;s included</h4>
           <div class="h-px flex-auto bg-gray-100"></div>
@@ -208,19 +208,19 @@ export function Pricing() {
             <svg class="h-6 w-5 flex-none text-black" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
             </svg>
-            Stylized emails
+            Delivered via stylized email sent from you
           </li>
           <li class="flex gap-x-3">
             <svg class="h-6 w-5 flex-none text-black" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
             </svg>
-            Spaced out delivery (daily)
+            Scheduled delivery 
             </li>
           <li class="flex gap-x-3">
             <svg class="h-6 w-5 flex-none text-black" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
             </svg>
-            Personalized messaging from you
+            Personalized messaging (from you)
           </li>
         </ul>
       </div>
