0f2b4e56974e9edc3ab5cb6a2c4acfda015784e9
dcgleason <danny.c.gleason@gmail.com>
2023-10-03 07:52:54 -0400

    update to pricing

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -187,15 +187,15 @@ export function Price() {
       <div class="bg-white py-24 sm:py-32">
   <div class="mx-auto max-w-7xl px-6 lg:px-8">
     <div class="mx-auto max-w-2xl sm:text-center">
-      <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Payment built around you.</h2>
-      <p class="mt-6 text-lg leading-8 text-gray-600">Starting $10 per month</p>
+      <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Flexible Payment Options Tailored to Your Needs.</h2>
+      <p class="mt-6 text-lg leading-8 text-gray-600">Plans start at just $10/month.</p>
     </div>
     <div class="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
       <div class="p-8 sm:p-10 lg:flex-auto">
-        <h3 class="text-2xl font-bold tracking-tight text-gray-900">1 gift book</h3>
-        <p class="mt-6 text-base leading-7 text-gray-600">Automated, stylized, and built to instill a sense of belonging.</p>
+        <h3 class="text-2xl font-bold tracking-tight text-gray-900">Single Keepsake Book</h3>
+        <p class="mt-6 text-base leading-7 text-gray-600">Automated, elegantly designed, and purpose-built to enhance team cohesion.</p>
         <div class="mt-10 flex items-center gap-x-4">
-          <h4 class="flex-none text-sm font-semibold leading-6 text-black">What&apos;s included</h4>
+          <h4 class="flex-none text-sm font-semibold leading-6 text-black">Included Features</h4>
           <div class="h-px flex-auto bg-gray-100"></div>
         </div>
         <ul role="list" class="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6">
@@ -203,25 +203,25 @@ export function Price() {
             <svg class="h-6 w-5 flex-none text-black" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
             </svg>
-            Automated submission collection
+            Automated collection of contributions
           </li>
           <li class="flex gap-x-3">
             <svg class="h-6 w-5 flex-none text-black" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
             </svg>
-            Hardcover book with your company logo
+            Custom hardcover book featuring your company logo
           </li>
           <li class="flex gap-x-3">
             <svg class="h-6 w-5 flex-none text-black" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
             </svg>
-            Scheduled delivery 
+            Timely, scheduled delivery
             </li>
           <li class="flex gap-x-3">
             <svg class="h-6 w-5 flex-none text-black" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
             </svg>
-            Personalized messaging (from you)
+            Your personalized messages included
           </li>
         </ul>
       </div>
@@ -233,7 +233,7 @@ export function Price() {
                 {/* <span class="text-2xl font-bold text-gray-900">FREE</span> */}
               </p>
               <a href="#" class="mt-10 block w-full rounded-md bg-[#DF9496] px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-600">Download app</a>
-              <p class="mt-6 text-xs leading-5 text-gray-600">Pay only for what you give.</p>
+              <p class="mt-6 text-xs leading-5 text-gray-600">Pay only for the gifts you actually send.</p>
             </div>
           </div>
         </div>
