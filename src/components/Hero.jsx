4397a46f2c34160fd07caac5399d8f253f94847e
dcgleason <danny.c.gleason@gmail.com>
2023-10-06 11:42:22 -0400

    update cropped pdf

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -168,7 +168,7 @@ export function Hero() {
           <div className="relative mt-10 sm:mt-20 lg:col-span-5 lg:row-span-2 lg:mt-0 xl:col-span-6 h-[800px] -translate-y-96"> {/* Added -translate-y-24 */}
             <BackgroundIllustration className="absolute left-1/2 top-4 h-[1026px] w-[1026px] -translate-x-1/3 stroke-gray-300/70 [mask-image:linear-gradient(to_bottom,white_20%,transparent_75%)] sm:top-16 sm:-translate-x-1/2 lg:-top-16 lg:ml-12 xl:-top-14 xl:ml-0" />
             <div className="mx-4 px-9 h-full z-50 [mask-image:linear-gradient(to_bottom,white_95%,transparent)] sm:mx-0 lg:absolute lg:-inset-x-10 lg:bottom-auto lg:-top-20 lg:h-auto lg:px-0 xl:bottom-0"> {/* Kept h-full */}
-              <DynamicPDFViewer fileUrl="/mom-bundl-interior-cropped.pdf" />
+              <DynamicPDFViewer fileUrl="/mom-bundl-interior-cropped-real.pdf" />
             </div>
           </div>
 
