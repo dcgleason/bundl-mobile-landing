dd34cfb4877aca22ece88471629f00efc14a9baf
dcgleason <danny.c.gleason@gmail.com>
2023-10-06 12:53:28 -0400

    update need to fiture this out

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -143,7 +143,7 @@ function PlayIcon(props) {
 
 export function Hero() {
   return (
-    <div className=" py-20 sm:py-32 lg:pb-32 xl:pb-36">
+    <div className="py-20 sm:py-32 lg:pb-32 xl:pb-36">
       <Container>
         <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20">
           <div className="relative z-10 mx-auto max-w-2xl lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6">
@@ -165,9 +165,9 @@ export function Hero() {
               </Button> */}
             </div>
           </div>
-          <div className="relative mt-10 sm:mt-20 lg:col-span-5 lg:row-span-2 lg:mt-0 xl:col-span-6 h-[800px] -translate-y-96"> {/* Added -translate-y-24 */}
+          <div className="relative sm:mt-20 lg:col-span-5 lg:row-span-2 lg:mt-0 xl:col-span-6 "> 
             <BackgroundIllustration className="absolute left-1/2 top-4 h-[1026px] w-[1026px] -translate-x-1/3 stroke-gray-300/70 [mask-image:linear-gradient(to_bottom,white_20%,transparent_75%)] sm:top-16 sm:-translate-x-1/2 lg:-top-16 lg:ml-12 xl:-top-14 xl:ml-0" />
-            <div className="mx-4 px-9 z-50 [mask-image:linear-gradient(to_bottom,white_95%,transparent)] sm:mx-0 lg:absolute lg:-inset-x-10 lg:bottom-auto lg:-top-20 lg:h-auto lg:px-0 xl:bottom-0"> {/* Kept h-full */}
+            <div className="mx-4 px-9 [mask-image:linear-gradient(to_bottom,white_95%,transparent)] sm:mx-0 lg:absolute lg:-inset-x-10 lg:bottom-auto lg:-top-20 lg:h-auto lg:px-0 xl:bottom-0"> {/* Kept h-full */}
               <DynamicPDFViewer fileUrl="/mom-bundl-interior-cropped-real.pdf" />
             </div>
           </div>
--- src/components/PDFViewer.jsx
+++ src/components/PDFViewer.jsx
@@ -2,12 +2,12 @@ import { Worker, Viewer, ScrollMode } from '@react-pdf-viewer/core';
 
 const PDFViewer = ({ fileUrl }) => {
   return (
-    <div style={{ width: '100%', height: '100vh' }}>
+    <div style={{ width: '100%', height: '100vh', overflow: 'auto' }}>
       <Worker workerUrl={`https://unpkg.com/pdfjs-dist@2.16.105/build/pdf.worker.min.js`}>
         <Viewer
           fileUrl={fileUrl}
           defaultScale={1}
-          scrollMode={ScrollMode.Horizontal}
+          scrollMode={ScrollMode.Vertical}
           defaultPage={0}
         />
       </Worker>
