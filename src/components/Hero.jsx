31a13bbf1ae11a3e40ca27bb266221edd89fef52
dcgleason <danny.c.gleason@gmail.com>
2023-10-06 07:54:12 -0400

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -148,7 +148,7 @@ export function Hero() {
         <div className="lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20">
           <div className="relative z-10 mx-auto max-w-2xl lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6">
             <h1 className="text-4xl font-medium tracking-tight text-gray-900">
-           Your most meaningful gift ... ever. 
+           Make your loved one cry try tears of joy.
            </h1>
             <p className="mt-6 text-lg text-gray-600">
             Blow your loved one away by gifting them heartwarming messages from their loved ones...all at once. <br></br> <br></br>With Bundl&apos;s mobile app, easily gift meaningful, prompt-driven written notes, audio-clips, and memorable pictures from your loved one&apos;s loved ones in the from of a book.  Great for to milestone birthdays, baby-showers, retirement parties, etc.
