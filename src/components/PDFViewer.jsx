467cfffd8bca6a143d4709dc049eecee950bfe51
dcgleason <danny.c.gleason@gmail.com>
2023-10-06 11:48:50 -0400

    update

--- src/components/PDFViewer.jsx
+++ src/components/PDFViewer.jsx
@@ -3,7 +3,7 @@ import { Worker, Viewer, ScrollMode } from '@react-pdf-viewer/core';
 const PDFViewer = ({ fileUrl }) => {
   return (
     <Worker workerUrl={`https://unpkg.com/pdfjs-dist@2.16.105/build/pdf.worker.min.js`}>
-      <Viewer fileUrl={fileUrl} defaultScale={1} scrollMode={ScrollMode.Vertical} />
+      <Viewer fileUrl={fileUrl} defaultScale={1} scrollMode={ScrollMode.Vertical} defaultPage={0} />
     </Worker>
   );
 };
