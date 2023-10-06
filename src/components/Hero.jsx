7481a81f8a743724f4091b4e4a9b7054db33b87d
dcgleason <danny.c.gleason@gmail.com>
2023-10-06 15:50:07 -0400

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,6 +23,8 @@ import logoWired from '@/images/logos/wired.svg'
 import BundlBook from "@/images/bundl-red-book.png"
 import dynamic from 'next/dynamic';
 import PDFViewer from './PDFViewer'
+import '../styles/pdf.css';
+
 
 import { Viewer, Worker } from '@react-pdf-viewer/core';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
@@ -196,12 +198,11 @@ export function Hero() {
             <BackgroundIllustration className="absolute left-1/2 top-4 h-[1026px] w-[1026px] -translate-x-1/3 stroke-gray-300/70 [mask-image:linear-gradient(to_bottom,white_20%,transparent_75%)] sm:top-16 sm:-translate-x-1/2 lg:-top-16 lg:ml-12 xl:-top-14 xl:ml-0" />
             <div className="mx-4 px-9 [mask-image:linear-gradient(to_bottom,white_95%,transparent)] sm:mx-0 lg:absolute lg:-inset-x-10 lg:bottom-auto lg:-top-20 lg:h-auto lg:px-0 xl:bottom-0"> Kept h-full */}
 
-          <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.js">
-              {/* Reference the style object within the style attribute of the div tag */}
-              <div style={style}>
-                  <Viewer fileUrl="/mom-bundl-interior-cropped-real.pdf" />
-              </div>
-          </Worker>      
+        <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.js">
+            <div className="container">
+                <Viewer fileUrl="/mom-bundl-interior-cropped-real.pdf" />
+            </div>
+        </Worker>  
             
           {/* </div>
           </div> */}
